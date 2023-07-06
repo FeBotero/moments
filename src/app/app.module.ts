8ee773abe95d8f855d8745cbee2b52e4aad79897
FeBotero <frpbotero@gmail.com>
2023-07-06 12:01:29 -0400

    Firts Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { NewComponentComponent } from './components/new-component/new-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    NewComponentComponent,
+    ParentDataComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.scss']
+})
+export class FirstComponentComponent {
+  name:string = "Felipe";
+  address={
+    street:"Franz Schubbert",
+    number:840,
+  }
+
+}
